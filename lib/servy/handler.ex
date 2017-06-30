8a1f09c625b390e7d39db0313fb9432fb72b96de
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-30 21:59:45 +0800

    Using Regular Expressions 匹配路由

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -28,9 +28,17 @@ defmodule Servy.Handler do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/" <> id}
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<things>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
   end
+
+  def rewrite_path_captures(conv, %{"things" => things, "id" => id}) do
+    %{conv | path: "/#{things}/#{id}"}
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
 
   def rewrite_path(conv), do: conv
 
