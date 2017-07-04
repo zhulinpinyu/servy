9c1c0934f78bae7d66ea075647b8d09de519ed51
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-05 06:44:42 +0800

    Exercise: Triple All The Numbers v1

--- lib/recurse.ex
+++ lib/recurse.ex
@@ -5,6 +5,15 @@ defmodule Recurse do
   end
 
   def loopy([], sum), do: sum
+
+  def triple([h|t], ret) do
+    ret = ret ++ [ h * 3 ]
+    triple(t, ret)
+  end
+
+  def triple([], ret), do: ret
 end
 
 IO.puts Recurse.loopy([1, 2, 3, 4, 5], 0)
+
+IO.inspect Recurse.triple([1, 2, 3, 4, 5], [])
