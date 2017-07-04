50b901acb32dc43ab2bdbeb175f703593cb49804
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-04 23:15:04 +0800

    Recursion

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,14 +4,30 @@ defmodule Servy.Parser do
 
     [request_line|header_lines] = String.split(top, "\n")
 
+    headers = parse_headers(header_lines, %{})
     [method, path, _] = request_line |> String.trim |> String.split(" ")
 
-    params = params_string |> String.trim |> URI.decode_query
+    params = parse_params(headers["Content-Type"], params_string)
 
     %Servy.Conv{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
+
+  def parse_params("application/x-www-form-urlencoded", params_string) do
+    params_string |> String.trim |> URI.decode_query
+  end
+
+  def parse_params(_, _), do: %{}
+
+  def parse_headers([head | tail], headers) do
+    [key, value] = head |> String.split(": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  def parse_headers([], headers), do: headers
 end
